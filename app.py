729dac78feed3c4c1cbb61ccf81e8bbbcf0ae23c
Nainesh03 <dr.nainesh03@gmail.com>
2026-02-10 05:20:43 -0500

    Delete broken app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template from data.demo_data import rubrics
-app = Flask (__name__)
-@app.route("/")
-def index() :
-    return render_template("index.html", rubrics=rubrics)
-if __name__== "__main__":
-    app.run(host="0.0.0.0", port=5000) 
