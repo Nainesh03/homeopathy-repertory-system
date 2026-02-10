5648bd644ef8b24b1abaa6732e485fc222c204b9
Nainesh03 <dr.nainesh03@gmail.com>
2026-02-10 06:17:56 -0500

    clean rewrite app.py without smart characters

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from data.demo_data import rubrics
+import os
+app = Flask(__name__)
+@app.route("/")
+def index() "
+    return render_template ("index.html", rubrics=rubrics)
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
