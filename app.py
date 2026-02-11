28609f89785ea83ee26b96dedd762224a95df673
Nainesh03 <dr.nainesh03@gmail.com>
2026-02-11 05:56:13 -0500

    Clean imports and prepare registration system

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from models import db, Doctor
-import os
 from flask_login import LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 
