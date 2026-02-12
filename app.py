a31c6e7d375a20a729a3515756308786d7d817a2
Nainesh03 <dr.nainesh03@gmail.com>
2026-02-12 03:39:49 -0500

    Fix database table creation for production

--- app.py
+++ app.py
@@ -55,5 +55,12 @@ def register():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "supersecret"
+app.config{"SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLALchemy(app)
+
+withapp.app_context():
+    db.create_all()
